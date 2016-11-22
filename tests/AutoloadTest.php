061c655bce1e7f6301a276f0772713e7903a35b9
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-22 18:00:45 -0200

    Tests...

---
+++ tests/AutoloadTest.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once (__DIR__ . '/../_autoload/autoload.php');
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+new One();
+new Two();
+new Three();
+new Four();
+new Five();
+new Six();
+
+echo "<br>";
+echo "<br>";
+
